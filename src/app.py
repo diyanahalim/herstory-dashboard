4c494edceb7bbf365a618ba94a2dc85083304e2e
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-13 22:58:33 +0800

    updated index.py, app.py and render.yaml

--- src/app.py
+++ src/app.py
@@ -7,4 +7,5 @@ external_stylesheets = [dbc.themes.MINTY]
 
 # Step 1. Launch the application
 app_dash = dash.Dash(__name__, external_stylesheets = external_stylesheets, suppress_callback_exceptions=True)
+server = app_dash.server  # Expose the server for deployment
 
--- src/index.py
+++ src/index.py
@@ -1,5 +1,5 @@
 from app import app_dash
-server = app_dash.server  # Expose the server for deployment
 
-if __name__ == "__main__":
-    app_dash.run_server(port=8086, debug=True, dev_tools_ui=False)
+# commented since not needed for deployment 
+# if __name__ == "__main__":
+#     app_dash.run_server(debug=False)
