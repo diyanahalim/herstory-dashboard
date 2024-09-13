c4673149f6dfab0ada889846f462da2355dc4d31
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-13 23:07:02 +0800

    updated index.py and render.yaml

--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
 from app import app_dash
 
 # commented since not needed for deployment 
-# if __name__ == "__main__":
-#     app_dash.run_server(debug=False)
+server = app_dash.server
+if __name__ == "__main__":
+    app_dash.run_server(debug=False)
