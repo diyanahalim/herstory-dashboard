4efcdb83fa3eb5b5ab44ddcb11f2746d506b21d2
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-14 10:41:36 +0800

    changes

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 
 import dash
 import dash_bootstrap_components as dbc
-from index import server
 
 external_stylesheets = [dbc.themes.MINTY]
 
@@ -9,5 +8,5 @@ external_stylesheets = [dbc.themes.MINTY]
 # Step 1. Launch the application
 app_dash = dash.Dash(__name__, external_stylesheets = external_stylesheets, suppress_callback_exceptions=True)
 # Optional: If you need to re-use the server, assign it to app_dash
-app_dash.server = server
+server = app_dash.server
 
