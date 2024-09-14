be94108cbd0237d7bd9ddbcf7cf20694dec6a7d9
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-14 10:19:13 +0800

    updated index.py

--- src/index.py
+++ src/index.py
@@ -1,8 +1,8 @@
 from app import app_dash
 
+server = app_dash.server
 # Expose the app_dash server for deployment
 # server = app_dash.server
 # Only expose app_dash; do not run the server here for deployment
 if __name__ == "__main__":
     app_dash.run_server(debug=False)
-server = app_dash.server
