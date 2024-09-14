5b8ff8a77275fcc82d2182f4729540683be0237a
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-14 10:32:02 +0800

    updated app.py and render.yaml

--- src/app.py
+++ src/app.py
@@ -1,12 +1,13 @@
 
 import dash
 import dash_bootstrap_components as dbc
+from index import server
 
 external_stylesheets = [dbc.themes.MINTY]
 
 
 # Step 1. Launch the application
 app_dash = dash.Dash(__name__, external_stylesheets = external_stylesheets, suppress_callback_exceptions=True)
-# Expose the server for deployment
-server = app_dash.server
+# Optional: If you need to re-use the server, assign it to app_dash
+app_dash.server = server
 
