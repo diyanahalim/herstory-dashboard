70b25f43d71272be21876727ff289ead3833e61b
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-14 12:26:50 +0800

    changes

--- src/index.py
+++ src/index.py
@@ -1,12 +1,12 @@
 
-from waitress import serve # Import the server directly
-from app import server
+#from waitress import serve  Import the server directly
+from app import app_dash
 
 # server = app_dash.server
 # Expose the app_dash server for deployment
-# server = app_dash.server
+server = app_dash.server
 # Only expose app_dash; do not run the server here for deployment
-# if __name__ == "__main__":
-#     app_dash.run_server(debug=False)
 if __name__ == "__main__":
-    serve(server, host='0.0.0.0', port=8080)
+    app_dash.run_server(debug=False)
+# if __name__ == "__main__":
+#     serve(server, host='0.0.0.0', port=8080)
