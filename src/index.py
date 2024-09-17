fab7435adae4e951a0a65ab41c6b90a6d9cadea8
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-17 16:25:01 +0800

    changes

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 # to test locally
 from waitress import serve
 from src.app import server
+from app import app_dash, server
 
 if __name__ == "__main__":
     serve(server, host='0.0.0.0', port=8080)
