c075aa272a35bb03f8a309130638b693b1bb427d
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-19 10:02:12 +0800

    changes

--- src/index.py
+++ src/index.py
@@ -1,4 +1,9 @@
 from src.app import app
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 server = app.server  # Expose the server for deployment
 
