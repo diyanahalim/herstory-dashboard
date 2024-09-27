775d78440c25e49f4c9525b2fc1a66b23291ebd8
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-27 10:56:30 +0800

    changes

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ external_stylesheets = [dbc.themes.MINTY]
 
 
 # Step 1. Launch the application
-app = dash.Dash(__name__, external_stylesheets = external_stylesheets, suppress_callback_exceptions=True)
+app = dash.Dash(__name__, title="HerStory", external_stylesheets = external_stylesheets, suppress_callback_exceptions=True)
 # Optional: If you need to re-use the server, assign it to app_dash
 # server = app_dash.server
 server = app.server
